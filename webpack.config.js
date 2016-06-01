654de93b15bd099cd2946c9dab413c93be7997a2
Kushal Pandya <kushalpandya@outlook.com>
2016-06-01 14:18:18 +0530

    Webpack Configuration

---
+++ webpack.config.js
@@ -0,0 +1,10 @@
+/**
+ * Poster v0.1.0
+ * A React webapp to list upcoming movies and maintain a watchlist, powered by TMDb
+ *
+ * Author: Kushal Pandya <kushalspandya@gmail.com> (https://doublslash.com)
+ * Date: 01 June, 2016
+ * License: MIT
+ *
+ * Webpack Configuration
+ */
