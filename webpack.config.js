045ef514f0767bc528b79d772ca3bf242d1dcc6f
Kushal Pandya <kushalpandya@outlook.com>
2016-06-01 18:10:07 +0530

    Exclusion regex updated.

--- webpack.config.js
+++ webpack.config.js
@@ -20,7 +20,7 @@ module.exports = {
         loaders: [
             {
                 test: /\.js?$/,
-                exclude: /(node_modules)/,
+                exclude: /node_modules/,
                 loader: 'babel-loader',
                 query: {
                     presets: [
