43dfc04f591b72cf66d955cd3752be0ba0d9d8d2
Kushal Pandya <kushalpandya@outlook.com>
2016-06-01 16:26:50 +0530

    Added initial Webpack Configuration.

--- webpack.config.js
+++ webpack.config.js
@@ -8,3 +8,42 @@
  *
  * Webpack Configuration
  */
+
+var webpack = require('webpack'),
+    debug = process.env.NODE_ENV !== "production";
+
+module.exports = {
+    context: __dirname + "/app",
+    devtool: debug ? "inline-sourcemap" : null,
+    entry: "./app.js",
+    module: {
+        loaders: [
+            {
+                test: /\.js?$/,
+                exclude: /(node_modules)/,
+                loader: 'babel-loader',
+                query: {
+                    presets: [
+                        'react',
+                        'es2015',
+                        'stage-0'
+                    ],
+                    plugins: [
+                        'react-html-attrs',
+                        'transform-class-properties',
+                        'transform-decorators-legacy'
+                    ]
+                }
+            }
+        ]
+    },
+    output: {
+        path: __dirname + "/app",
+        filename: "app.min.js"
+    },
+    plugins: debug ? [] : [
+        new webpack.optimize.DedupePlugin(),
+        new webpack.optimize.OccurenceOrderPlugin(),
+        new webpack.optimize.UglifyJsPlugin({ mangle: false, sourcemap: false })
+    ]
+};
