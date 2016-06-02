0a5be558f48c65adaafe08a009cf8d17230852d1
Kushal Pandya <kushalspandya@gmail.com>
2016-06-02 15:05:12 +0530

    Footer Parent Container.

---
+++ app/components/Footer/Footer.js
@@ -0,0 +1,25 @@
+/**
+ * Poster v0.1.0
+ * A React webapp to list upcoming movies and maintain a watchlist, powered by TMDb
+ *
+ * Author: Kushal Pandya <kushalspandya@gmail.com> (https://doublslash.com)
+ * Date: 01 June, 2016
+ * License: MIT
+ *
+ * Footer Parent Container
+ */
+
+import React from "react";
+
+export default
+class Footer extends React.Component {
+    constructor() {
+        super();
+    }
+
+    render() {
+        return (
+            <footer>This is from footer</footer>
+        );
+    }
+}
