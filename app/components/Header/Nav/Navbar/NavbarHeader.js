e72da1792d599104b44e113bb144298a76e6e792
Kushal Pandya <kushalspandya@gmail.com>
2016-06-02 15:06:49 +0530

    Navbar Header Container.

---
+++ app/components/Header/Nav/Navbar/NavbarHeader.js
@@ -0,0 +1,31 @@
+/**
+ * Poster v0.1.0
+ * A React webapp to list upcoming movies and maintain a watchlist, powered by TMDb
+ *
+ * Author: Kushal Pandya <kushalspandya@gmail.com> (https://doublslash.com)
+ * Date: 02 June, 2016
+ * License: MIT
+ *
+ * Navbar Header Container
+ */
+
+import React from "react";
+
+import NavbarToggle from "./NavbarToggle";
+import NavbarBrand from "./NavbarBrand";
+
+export default
+class NavbarHeader extends React.Component {
+    constructor() {
+        super();
+    }
+
+    render() {
+        return (
+            <div class="navbar-header">
+                <NavbarToggle/>
+                <NavbarBrand/>
+            </div>
+        );
+    }
+}
