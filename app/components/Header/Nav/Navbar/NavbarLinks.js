23f00e02944a9785b795957ddb34a7247c0447e3
Kushal Pandya <kushalspandya@gmail.com>
2016-06-02 15:08:22 +0530

    Navbar Links Container.

---
+++ app/components/Header/Nav/Navbar/NavbarLinks.js
@@ -0,0 +1,30 @@
+/**
+ * Poster v0.1.0
+ * A React webapp to list upcoming movies and maintain a watchlist, powered by TMDb
+ *
+ * Author: Kushal Pandya <kushalspandya@gmail.com> (https://doublslash.com)
+ * Date: 02 June, 2016
+ * License: MIT
+ *
+ * Navbar Links Container
+ */
+
+import React from "react";
+
+import NavbarLinkItem from "./NavbarLinkItem";
+
+export default
+class NavbarLinks extends React.Component {
+    constructor() {
+        super();
+    }
+
+    render() {
+        return (
+            <ul class="nav navbar-nav">
+                <NavbarLinkItem linkTitle="Upcoming Movies"/>
+                <NavbarLinkItem linkTitle="Watchlist"/>
+            </ul>
+        );
+    }
+}
