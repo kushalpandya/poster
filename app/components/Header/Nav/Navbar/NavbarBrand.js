cb4f2dd44fbf850ee30d52505d059bc6aae4cea7
Kushal Pandya <kushalspandya@gmail.com>
2016-06-02 15:06:20 +0530

    Navbar Brand Component.

---
+++ app/components/Header/Nav/Navbar/NavbarBrand.js
@@ -0,0 +1,25 @@
+/**
+ * Poster v0.1.0
+ * A React webapp to list upcoming movies and maintain a watchlist, powered by TMDb
+ *
+ * Author: Kushal Pandya <kushalspandya@gmail.com> (https://doublslash.com)
+ * Date: 02 June, 2016
+ * License: MIT
+ *
+ * Navbar Brand Component
+ */
+
+import React from "react";
+
+export default
+class NavbarBrand extends React.Component {
+    constructor() {
+        super();
+    }
+
+    render() {
+        return (
+            <a class="navbar-brand" href="/">Poster</a>
+        );
+    }
+}
