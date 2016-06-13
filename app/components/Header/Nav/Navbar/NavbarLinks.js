037bfa493c04c5823022682ea00d7d9b2a88f000
Kushal Pandya <kushalspandya@gmail.com>
2016-06-13 10:20:58 +0530

    Added "Top 20" link for showing top rated movies.

--- app/components/Header/Nav/Navbar/NavbarLinks.js
+++ app/components/Header/Nav/Navbar/NavbarLinks.js
@@ -22,6 +22,7 @@ class NavbarLinks extends React.Component {
     render() {
         return (
             <ul class="nav navbar-nav">
+                <NavbarLinkItem linkTitle="Top 20"/>
                 <NavbarLinkItem linkTitle="Upcoming Movies"/>
                 <NavbarLinkItem linkTitle="Watchlist"/>
             </ul>
