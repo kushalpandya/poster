ba9b8f031b8390fbbbed05dd191afa25dff5d987
Kushal Pandya <kushalspandya@gmail.com>
2016-06-13 14:32:26 +0530

    Added routing support for various sections.

--- app/app.js
+++ app/app.js
@@ -9,23 +9,48 @@
  * React App Entry-point
  */
 
+/*
+ * Library
+ */
 import React from "react";
 import ReactDOM from "react-dom";
+import { Router, Route, IndexRoute, hashHistory } from "react-router";
 
+/*
+ * Persistent Page Components
+ */
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
 
+/*
+ * Dynamic Page Components
+ */
+import Home from "./components/Section/Home/Home";
+import TopRated from "./components/Section/TopRated/TopRated";
+import Upcoming from "./components/Section/Upcoming/Upcoming";
+import Watchlist from "./components/Section/Watchlist/Watchlist";
+
+export default
 class App extends React.Component {
     render() {
         return (
             <div>
                 <Header/>
+                {this.props.children}
                 <Footer/>
             </div>
         );
     }
 }
 
-const appEl = document.getElementById('poster-app');
-
-ReactDOM.render(<App/>, appEl);
+ReactDOM.render(
+    <Router history={hashHistory}>
+        <Route path="/" component={App}>
+            <IndexRoute component={Home}></IndexRoute>
+            <Route path="top_rated" component={TopRated}></Route>
+            <Route path="upcoming" component={Upcoming}></Route>
+            <Route path="watchlist" component={Watchlist}></Route>
+        </Route>
+    </Router>,
+    document.getElementById('poster-app')
+);
