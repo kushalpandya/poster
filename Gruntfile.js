307bda85611a6e3a15e51a9062598f5f45eaa898
Kushal Pandya <kushalspandya@gmail.com>
2016-06-14 20:26:51 +0530

    SVGs are also minified during build.

--- Gruntfile.js
+++ Gruntfile.js
@@ -140,7 +140,7 @@ module.exports = function(grunt) {
                         expand: true,
                         cwd: directories.imagesDir,
                         dest: directories.generatedImagesDir,
-                        src: ['**/*.{png,jpg,gif}']
+                        src: ['**/*.{png,jpg,gif,svg}']
                     }
                 ]
             }
