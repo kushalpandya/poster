ecd1c257dd1e341f1b6dd8ea28ebd2cb30d6b3ab
Kushal Pandya <kushalspandya@gmail.com>
2016-06-14 20:27:27 +0530

    Loading Message Component.

---
+++ app/components/Views/Section/Shared/LoadingMessage.js
@@ -0,0 +1,28 @@
+/**
+ * Poster v0.1.0
+ * A React webapp to list upcoming movies and maintain a watchlist, powered by TMDb
+ *
+ * Author: Kushal Pandya <kushalspandya@gmail.com> (https://doublslash.com)
+ * Date: 14 June, 2016
+ * License: MIT
+ *
+ * Section > * -> [ LoadingMessage ]
+ */
+
+import React from "react";
+
+export default
+class LoadingMessage extends React.Component {
+    constructor() {
+        super();
+    }
+
+    render() {
+        return (
+            <div class={"loading-message " + (this.props.loadCompleted ? "hidden" : "")}>
+                <img src="assets/images/hourglass.svg"/>
+                <h5 class="text-muted">{this.props.loadMessage}</h5>
+            </div>
+        );
+    }
+}
