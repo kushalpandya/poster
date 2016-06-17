5c5047647b3bd5ce52a1433ca6de18d07e7afb34
Kushal Pandya <kushalspandya@gmail.com>
2016-06-18 01:20:40 +0530

    Movie Cards List Component.

---
+++ app/components/Views/Section/Shared/MovieCardList.js
@@ -0,0 +1,35 @@
+/**
+ * Poster v0.1.0
+ * A React webapp to list upcoming movies and maintain a watchlist, powered by TMDb
+ *
+ * Author: Kushal Pandya <kushalspandya@gmail.com> (https://doublslash.com)
+ * Date: 13 June, 2016
+ * License: MIT
+ *
+ * Section >  * > [ Movie Cards List ]
+ */
+
+import React from "react";
+
+import MovieCard from "../Shared/MovieCard";
+
+export default
+class MovieCardList extends React.Component {
+    constructor() {
+        super();
+    }
+
+    render() {
+        return (
+            <div class="movies-list">
+                {
+                    this.props.movies.map((movie, i) => {
+                        return (
+                            <MovieCard key={movie.id} movie={movie} />
+                        );
+                    })
+                }
+            </div>
+        );
+    }
+}
