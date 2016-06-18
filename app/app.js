993c85d478ab102f91fe56c7f68c0481a614916f
Kushal Pandya <kushalspandya@gmail.com>
2016-06-18 12:52:03 +0530

    Page base layout moved to Home component.

--- app/app.js
+++ app/app.js
@@ -17,41 +17,18 @@ import ReactDOM from "react-dom";
 import { Router, Route, IndexRoute, hashHistory } from "react-router";
 
 /*
- * Persistent Page Components
+ * Page Components
  */
-import Header from "./components/Views/Header/Header";
-import Footer from "./components/Views/Footer/Footer";
-
-/*
- * Dynamic Page Components
- */
-import Home from "./components/Views/Section/Home/Home";
-import TopRated from "./components/Views/Section/TopRated/TopRated";
-import Upcoming from "./components/Views/Section/Upcoming/Upcoming";
-import Watchlist from "./components/Views/Section/Watchlist/Watchlist";
-import MovieDetail from "./components/Views/Section/Shared/MovieDetail";
-
-import SearchResults from "./components/Views/Section/Home/SearchResults";
-
-export default
-class App extends React.Component {
-    render() {
-        return (
-            <div>
-                <Header/>
-                <section class="container poster-section">
-                    <Home/>
-                    {this.props.children}
-                </section>
-                <Footer/>
-            </div>
-        );
-    }
-}
+import Home             from "./components/Views/Home";
+import SearchResults    from "./components/Views/Section/SearchResults";
+import TopRated         from "./components/Views/Section/TopRated/TopRated";
+import Upcoming         from "./components/Views/Section/Upcoming/Upcoming";
+import Watchlist        from "./components/Views/Section/Watchlist/Watchlist";
+import MovieDetail      from "./components/Views/Section/Shared/MovieDetail";
 
 ReactDOM.render(
     <Router history={hashHistory}>
-        <Route path="/" component={App}>
+        <Route path="/" component={Home}>
             <Route path="search" component={SearchResults}></Route>
             <Route path="top_rated" component={TopRated}></Route>
             <Route path="upcoming" component={Upcoming}></Route>
