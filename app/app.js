c470bf4852d782ed9be4dad3eaddb7f8831379c2
Kushal Pandya <kushalpandya@outlook.com>
2016-06-21 18:56:37 +0530

    Added Redux store provider.

--- app/app.js
+++ app/app.js
@@ -15,6 +15,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { Router, Route, IndexRoute, hashHistory } from "react-router";
+import { Provider } from "react-redux";
 
 /*
  * Page Components
@@ -26,15 +27,22 @@ import Upcoming         from "./components/Views/Section/Upcoming/Upcoming";
 import Watchlist        from "./components/Views/Section/Watchlist/Watchlist";
 import MovieDetail      from "./components/Views/Section/MovieDetail/MovieDetail";
 
+/*
+ * Poster Redux Store
+ */
+import Store from "./store";
+
 ReactDOM.render(
-    <Router history={hashHistory}>
-        <Route path="/" component={Home}>
-            <Route path="search" component={SearchResults}></Route>
-            <Route path="top_rated" component={TopRated}></Route>
-            <Route path="upcoming" component={Upcoming}></Route>
-            <Route path="watchlist" component={Watchlist}></Route>
-            <Route path="movie/:movieId" component={MovieDetail}></Route>
-        </Route>
-    </Router>,
+    <Provider store={Store}>
+        <Router history={hashHistory}>
+            <Route path="/" component={Home}>
+                <Route path="search" component={SearchResults}></Route>
+                <Route path="top_rated" component={TopRated}></Route>
+                <Route path="upcoming" component={Upcoming}></Route>
+                <Route path="watchlist" component={Watchlist}></Route>
+                <Route path="movie/:movieId" component={MovieDetail}></Route>
+            </Route>
+        </Router>
+    </Provider>,
     document.getElementById('poster-app')
 );
