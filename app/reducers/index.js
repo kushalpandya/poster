6f312d31dcbdfa42f4f0326a12bf695ff1b2b9b0
Kushal Pandya <kushalpandya@outlook.com>
2016-06-23 16:11:40 +0530

    Remove watchlistReducer.

--- app/reducers/index.js
+++ app/reducers/index.js
@@ -11,11 +11,9 @@
 
 import { combineReducers } from "redux";
 
-import watchlistReducer from "./watchlist";
 import posterReducer    from "./poster";
 
 let posterApp = combineReducers({
-    watchlist: watchlistReducer,
     poster: posterReducer
 });
 
